2013ab200ad0a84232e3d9e2b71f8c840c5b2afb
SePhII2oTh <ipedrero84@gmail.com>
2022-04-01 19:25:53 -0600

    Update build-ubuntu-20.04.sh

--- build-ubuntu-20.04.sh
+++ build-ubuntu-20.04.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # add dotnet repo
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
 # install dev-dependencies
 sudo apt-get update; \
-  sudo apt-get -y install dotnet-sdk-6.0 git cmake build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5
+  sudo apt-get -y install dotnet-sdk-6.0 git cmake build-essential libcurl3 openssl1.0-dev pkg-config libboost-all-dev libsodium-dev libzmq5
 
 (cd src/Miningcore && \
 BUILDIR=${1:-../../build} && \
