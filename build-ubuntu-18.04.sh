40e17d4c6eb9d8241f742432252e3f3798961dfe
SePhII2oTh <ipedrero84@gmail.com>
2022-04-01 19:37:47 -0600

    add autoconf

--- build-ubuntu-18.04.sh
+++ build-ubuntu-18.04.sh
@@ -7,7 +7,7 @@ rm packages-microsoft-prod.deb
 
 # install dev-dependencies
 sudo apt-get update; \
-  sudo apt-get -y install dotnet-sdk-6.0 git cmake build-essential libcurl3 openssl1.0-dev pkg-config libboost-all-dev libsodium-dev libzmq5
+  sudo apt-get -y install dotnet-sdk-6.0 git cmake build-essential libcurl3 openssl1.0-dev pkg-config libboost-all-dev libsodium-dev libzmq5 autoconf 
 
 (cd src/Miningcore && \
 BUILDIR=${1:-../../build} && \
